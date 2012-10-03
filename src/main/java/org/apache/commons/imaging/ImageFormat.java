47c12a61ea480886c857005075f0f2e3d001f877
Damjan Jovanovic <damjan@apache.org>
2012-10-03 18:46:10 +0000

    Clean up ImageFormats.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/imaging/trunk@1393663 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/imaging/ImageFormat.java
+++ src/main/java/org/apache/commons/imaging/ImageFormat.java
@@ -68,27 +68,29 @@ public class ImageFormat {
 
     public static final ImageFormat IMAGE_FORMAT_UNKNOWN = new ImageFormat(
             "UNKNOWN", false);
-    public static final ImageFormat IMAGE_FORMAT_PNG = new ImageFormat("PNG");
-    public static final ImageFormat IMAGE_FORMAT_GIF = new ImageFormat("GIF");
-    public static final ImageFormat IMAGE_FORMAT_ICO = new ImageFormat("ICO");
-    public static final ImageFormat IMAGE_FORMAT_TIFF = new ImageFormat("TIFF");
-    public static final ImageFormat IMAGE_FORMAT_JPEG = new ImageFormat("JPEG");
     public static final ImageFormat IMAGE_FORMAT_BMP = new ImageFormat("BMP");
+    public static final ImageFormat IMAGE_FORMAT_DCX = new ImageFormat("DCX");
+    public static final ImageFormat IMAGE_FORMAT_GIF = new ImageFormat("GIF");
+    public static final ImageFormat IMAGE_FORMAT_ICNS = new ImageFormat("ICNS");
+    public static final ImageFormat IMAGE_FORMAT_ICO = new ImageFormat("ICO");
+    public static final ImageFormat IMAGE_FORMAT_JBIG2 = new ImageFormat("JBig2");
+    public static final ImageFormat IMAGE_FORMAT_JPEG = new ImageFormat("JPEG");
+    public static final ImageFormat IMAGE_FORMAT_PAM = new ImageFormat("PAM");
     public static final ImageFormat IMAGE_FORMAT_PSD = new ImageFormat("PSD");
     public static final ImageFormat IMAGE_FORMAT_PBM = new ImageFormat("PBM");
     public static final ImageFormat IMAGE_FORMAT_PGM = new ImageFormat("PGM");
-    public static final ImageFormat IMAGE_FORMAT_PPM = new ImageFormat("PPM");
     public static final ImageFormat IMAGE_FORMAT_PNM = new ImageFormat("PNM");
-    public static final ImageFormat IMAGE_FORMAT_TGA = new ImageFormat("TGA");
-    public static final ImageFormat IMAGE_FORMAT_JBIG2 = new ImageFormat(
-            "JBig2");
-    public static final ImageFormat IMAGE_FORMAT_ICNS = new ImageFormat("ICNS");
-    public static final ImageFormat IMAGE_FORMAT_WBMP = new ImageFormat("WBMP");
+    public static final ImageFormat IMAGE_FORMAT_PPM = new ImageFormat("PPM");
     public static final ImageFormat IMAGE_FORMAT_PCX = new ImageFormat("PCX");
-    public static final ImageFormat IMAGE_FORMAT_DCX = new ImageFormat("DCX");
+    public static final ImageFormat IMAGE_FORMAT_PNG = new ImageFormat("PNG");
+    public static final ImageFormat IMAGE_FORMAT_RGBE = new ImageFormat("RGBE");
+    public static final ImageFormat IMAGE_FORMAT_TGA = new ImageFormat("TGA");
+    public static final ImageFormat IMAGE_FORMAT_TIFF = new ImageFormat("TIFF");
+    public static final ImageFormat IMAGE_FORMAT_WBMP = new ImageFormat("WBMP");
     public static final ImageFormat IMAGE_FORMAT_XBM = new ImageFormat("XBM");
     public static final ImageFormat IMAGE_FORMAT_XPM = new ImageFormat("XPM");
-    public static final ImageFormat IMAGE_FORMAT_RGBE = new ImageFormat("RGBE");
+    
+    
 
     /**
      * Gets an array of ImageFormat objects for all supported image
@@ -96,13 +98,30 @@ public class ImageFormat {
      * @return A valid array of ImageFormat objects.
      */
     public static ImageFormat[] getAllFormats() {
-        ImageFormat result[] = { IMAGE_FORMAT_UNKNOWN, IMAGE_FORMAT_PNG,
-                IMAGE_FORMAT_GIF, IMAGE_FORMAT_TIFF, IMAGE_FORMAT_JPEG,
-                IMAGE_FORMAT_BMP, IMAGE_FORMAT_PSD, IMAGE_FORMAT_PBM,
-                IMAGE_FORMAT_PGM, IMAGE_FORMAT_PPM, IMAGE_FORMAT_PNM,
-                IMAGE_FORMAT_TGA, IMAGE_FORMAT_JBIG2, IMAGE_FORMAT_ICNS,
-                IMAGE_FORMAT_WBMP, IMAGE_FORMAT_PCX, IMAGE_FORMAT_DCX,
-                IMAGE_FORMAT_XBM, IMAGE_FORMAT_XPM, IMAGE_FORMAT_RGBE };
+        ImageFormat result[] = {
+                IMAGE_FORMAT_UNKNOWN,
+                IMAGE_FORMAT_BMP,
+                IMAGE_FORMAT_DCX,
+                IMAGE_FORMAT_GIF,
+                IMAGE_FORMAT_ICNS,
+                IMAGE_FORMAT_ICO,
+                IMAGE_FORMAT_JBIG2,
+                IMAGE_FORMAT_JPEG,
+                IMAGE_FORMAT_PNG,
+                IMAGE_FORMAT_PAM,
+                IMAGE_FORMAT_PSD,
+                IMAGE_FORMAT_PBM,
+                IMAGE_FORMAT_PCX,
+                IMAGE_FORMAT_PGM,
+                IMAGE_FORMAT_PNM,
+                IMAGE_FORMAT_PPM,
+                IMAGE_FORMAT_RGBE,
+                IMAGE_FORMAT_TGA,
+                IMAGE_FORMAT_TIFF, 
+                IMAGE_FORMAT_WBMP, 
+                IMAGE_FORMAT_XBM,
+                IMAGE_FORMAT_XPM,
+        };
 
         return result;
     }
