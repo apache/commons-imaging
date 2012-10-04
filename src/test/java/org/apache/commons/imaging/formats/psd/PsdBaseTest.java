4c4be402d91ae2b6bf423b3d2ea1639902ab55f8
Damjan Jovanovic <damjan@apache.org>
2012-10-04 20:50:53 +0000

    Add PSD tests.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/imaging/trunk@1394259 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/imaging/formats/psd/PsdBaseTest.java
@@ -0,0 +1,29 @@
+package org.apache.commons.imaging.formats.psd;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.imaging.ImageFormat;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.ImagingTest;
+
+public abstract class PsdBaseTest extends ImagingTest {
+    private static boolean isPsd(File file) throws IOException,
+            ImageReadException {
+        ImageFormat format = Imaging.guessFormat(file);
+        return format == ImageFormat.IMAGE_FORMAT_PSD;
+    }
+
+    private static final ImageFilter IMAGE_FILTER = new ImageFilter() {
+        public boolean accept(File file) throws IOException, ImageReadException {
+            return isPsd(file);
+        }
+    };
+
+    protected List<File> getPsdImages() throws IOException, ImageReadException {
+        return getTestImages(IMAGE_FILTER);
+    }
+
+}
---
+++ src/test/java/org/apache/commons/imaging/formats/psd/PsdReadTest.java
@@ -0,0 +1,40 @@
+package org.apache.commons.imaging.formats.psd;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.imaging.ImageInfo;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.common.IImageMetadata;
+import org.apache.commons.imaging.util.Debug;
+
+public class PsdReadTest extends PsdBaseTest {
+    public void test() throws Exception {
+        Debug.debug("start");
+
+        List<File> images = getPsdImages();
+        for (int i = 0; i < images.size(); i++) {
+            if (i % 10 == 0)
+                Debug.purgeMemory();
+
+            File imageFile = images.get(i);
+            Debug.debug("imageFile", imageFile);
+
+            IImageMetadata metadata = Imaging.getMetadata(imageFile);
+            // assertNotNull(metadata);
+
+            Map<String,Object> params = new HashMap<String,Object>();
+            ImageInfo imageInfo = Imaging.getImageInfo(imageFile, params);
+            assertNotNull(imageInfo);
+            
+            Imaging.getICCProfile(imageFile, params);
+
+            BufferedImage image = Imaging.getBufferedImage(imageFile);
+            assertNotNull(image);
+        }
+    }
+
+}
