9fb8666e008ae7fad5a77443da1e0924a2f3e771
Gary D. Gregory <ggregory@apache.org>
2013-10-24 00:09:05 +0000

    Sort members in AB order.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/imaging/trunk@1535219 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/imaging/util/IoUtils.java
+++ src/main/java/org/apache/commons/imaging/util/IoUtils.java
@@ -31,160 +31,6 @@ import java.nio.channels.FileChannel;
 import org.apache.commons.imaging.ImagingConstants;
 
 public class IoUtils implements ImagingConstants {
-    /**
-     * This class should never be instantiated.
-     */
-    private IoUtils() {
-    }
-
-    /**
-     * Reads an InputStream to the end.
-     * <p>
-     * 
-     * @param is
-     *            The InputStream to read.
-     * @return A byte array containing the contents of the InputStream
-     * @see InputStream
-     */
-    public static byte[] getInputStreamBytes(InputStream is) throws IOException {
-        ByteArrayOutputStream os = null;
-
-        try {
-            os = new ByteArrayOutputStream(4096);
-
-            is = new BufferedInputStream(is);
-
-            int count;
-            final byte[] buffer = new byte[4096];
-            while ((count = is.read(buffer, 0, 4096)) > 0) {
-                os.write(buffer, 0, count);
-            }
-
-            os.flush();
-
-            return os.toByteArray();
-        } finally {
-            try {
-                if (os != null) {
-                    os.close();
-                }
-            } catch (final IOException e) {
-                Debug.debug(e);
-            }
-        }
-    }
-
-    /**
-     * Reads a File into memory.
-     * <p>
-     * 
-     * @param file
-     *            The File to read.
-     * @return A byte array containing the contents of the File
-     * @see InputStream
-     */
-    public static byte[] getFileBytes(final File file) throws IOException {
-        InputStream is = null;
-
-        try {
-            is = new FileInputStream(file);
-
-            return getInputStreamBytes(is);
-        } finally {
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            } catch (final IOException e) {
-                Debug.debug(e);
-            }
-        }
-    }
-
-    public static void writeToFile(final byte[] src, final File file) throws IOException {
-        ByteArrayInputStream stream = null;
-
-        try {
-            stream = new ByteArrayInputStream(src);
-
-            putInputStreamToFile(stream, file);
-        } finally {
-            try {
-                if (stream != null) {
-                    stream.close();
-                }
-            } catch (final Exception e) {
-                Debug.debug(e);
-
-            }
-        }
-    }
-
-    public static void putInputStreamToFile(final InputStream src, final File file)
-            throws IOException {
-        FileOutputStream stream = null;
-
-        try {
-            if (file.getParentFile() != null && !file.getParentFile().exists() &&
-                !file.getParentFile().mkdirs()) {
-                throw new IOException(
-                        "Could not create directory for file " + file);
-            }
-            stream = new FileOutputStream(file);
-
-            copyStreamToStream(src, stream);
-        } finally {
-            try {
-                if (stream != null) {
-                    stream.close();
-                }
-            } catch (final Exception e) {
-                Debug.debug(e);
-            }
-        }
-    }
-
-    public static void copyStreamToStream(final InputStream src, final OutputStream dst)
-            throws IOException {
-        copyStreamToStream(src, dst, true);
-    }
-
-    public static void copyStreamToStream(final InputStream src, final OutputStream dst,
-            final boolean close_streams) throws IOException {
-        BufferedInputStream bis = null;
-        BufferedOutputStream bos = null;
-
-        try {
-            bis = new BufferedInputStream(src);
-            bos = new BufferedOutputStream(dst);
-
-            int count;
-            final byte[] buffer = new byte[4096];
-            while ((count = bis.read(buffer, 0, buffer.length)) > 0) {
-                dst.write(buffer, 0, count);
-            }
-            bos.flush();
-        } finally {
-            if (close_streams) {
-                try {
-                    if (bis != null) {
-                        bis.close();
-                    }
-                } catch (final IOException e) {
-                    Debug.debug(e);
-                }
-                try {
-                    if (bos != null) {
-                        bos.close();
-                    }
-                } catch (final IOException e) {
-                    Debug.debug(e);
-                }
-            }
-        }
-
-    }
-
     public static final boolean copyFileNio(final File src, final File dst)
             throws IOException {
         FileChannel srcChannel = null, dstChannel = null;
@@ -233,6 +79,160 @@ public class IoUtils implements ImagingConstants {
                 Debug.debug(e);
             }
         }
+    }
+
+    public static void copyStreamToStream(final InputStream src, final OutputStream dst)
+            throws IOException {
+        copyStreamToStream(src, dst, true);
+    }
+
+    public static void copyStreamToStream(final InputStream src, final OutputStream dst,
+            final boolean close_streams) throws IOException {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+
+        try {
+            bis = new BufferedInputStream(src);
+            bos = new BufferedOutputStream(dst);
+
+            int count;
+            final byte[] buffer = new byte[4096];
+            while ((count = bis.read(buffer, 0, buffer.length)) > 0) {
+                dst.write(buffer, 0, count);
+            }
+            bos.flush();
+        } finally {
+            if (close_streams) {
+                try {
+                    if (bis != null) {
+                        bis.close();
+                    }
+                } catch (final IOException e) {
+                    Debug.debug(e);
+                }
+                try {
+                    if (bos != null) {
+                        bos.close();
+                    }
+                } catch (final IOException e) {
+                    Debug.debug(e);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Reads a File into memory.
+     * <p>
+     * 
+     * @param file
+     *            The File to read.
+     * @return A byte array containing the contents of the File
+     * @see InputStream
+     */
+    public static byte[] getFileBytes(final File file) throws IOException {
+        InputStream is = null;
+
+        try {
+            is = new FileInputStream(file);
+
+            return getInputStreamBytes(is);
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (final IOException e) {
+                Debug.debug(e);
+            }
+        }
+    }
+
+    /**
+     * Reads an InputStream to the end.
+     * <p>
+     * 
+     * @param is
+     *            The InputStream to read.
+     * @return A byte array containing the contents of the InputStream
+     * @see InputStream
+     */
+    public static byte[] getInputStreamBytes(InputStream is) throws IOException {
+        ByteArrayOutputStream os = null;
+
+        try {
+            os = new ByteArrayOutputStream(4096);
+
+            is = new BufferedInputStream(is);
+
+            int count;
+            final byte[] buffer = new byte[4096];
+            while ((count = is.read(buffer, 0, 4096)) > 0) {
+                os.write(buffer, 0, count);
+            }
+
+            os.flush();
+
+            return os.toByteArray();
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch (final IOException e) {
+                Debug.debug(e);
+            }
+        }
+    }
+
+    public static void putInputStreamToFile(final InputStream src, final File file)
+            throws IOException {
+        FileOutputStream stream = null;
+
+        try {
+            if (file.getParentFile() != null && !file.getParentFile().exists() &&
+                !file.getParentFile().mkdirs()) {
+                throw new IOException(
+                        "Could not create directory for file " + file);
+            }
+            stream = new FileOutputStream(file);
+
+            copyStreamToStream(src, stream);
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+            } catch (final Exception e) {
+                Debug.debug(e);
+            }
+        }
+    }
+
+    public static void writeToFile(final byte[] src, final File file) throws IOException {
+        ByteArrayInputStream stream = null;
+
+        try {
+            stream = new ByteArrayInputStream(src);
+
+            putInputStreamToFile(stream, file);
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+            } catch (final Exception e) {
+                Debug.debug(e);
+
+            }
+        }
+    }
+
+    /**
+     * This class should never be instantiated.
+     */
+    private IoUtils() {
     }
 
 }
