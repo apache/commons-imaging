61ad6ffc62ad843511b2b1f8412a05a91b052287
Emmanuel Bourg <ebourg@apache.org>
2013-11-27 21:59:55 +0000

    Turned PcxConstants into an interface for consistency
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/imaging/trunk@1546207 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/imaging/formats/pcx/PcxConstants.java
+++ src/main/java/org/apache/commons/imaging/formats/pcx/PcxConstants.java
@@ -15,13 +15,10 @@
 package org.apache.commons.imaging.formats.pcx;
 
 
-public final class PcxConstants {
+public interface PcxConstants {
     public static final String PARAM_KEY_PCX_COMPRESSION = "PCX_COMPRESSION";
     public static final int PCX_COMPRESSION_UNCOMPRESSED = 0;
     public static final int PCX_COMPRESSION_RLE = 1;
 
     public static final String PARAM_KEY_PCX_BIT_DEPTH = "PCX_BIT_DEPTH";
-    
-    private PcxConstants() {
-    }
 }
