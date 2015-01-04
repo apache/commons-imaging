dab1aaaea3bb09cdf15a5aa7479cc5c757419f54
Benedikt Ritter <britter@apache.org>
2015-01-04 17:27:29 +0000

    Make ImagingGuessFormatTest a parameterized test
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/imaging/trunk@1649368 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/imaging/ImagingGuessFormatTest.java
+++ src/test/java/org/apache/commons/imaging/ImagingGuessFormatTest.java
@@ -22,10 +22,15 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.apache.commons.io.FilenameUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class ImagingGuessFormatTest extends ImagingTest {
 
     public static final String BMP_IMAGE_FILE = "bmp\\1\\Oregon Scientific DS6639 - DSC_0307 - small.bmp";
@@ -40,44 +45,47 @@ public class ImagingGuessFormatTest extends ImagingTest {
     public static final String PGM_IMAGE_FILE = "pbm\\1\\Oregon Scientific DS6639 - DSC_0307 - small.pgm";
     public static final String PPM_IMAGE_FILE = "pbm\\1\\Oregon Scientific DS6639 - DSC_0307 - small.ppm";
     public static final String TGA_IMAGE_FILE = "tga\\1\\Oregon Scientific DS6639 - DSC_0307 - small.tga";
-
-    @Test
-    public void testGuess_all() throws Exception {
-        testGuess(ImageFormats.PNG, PNG_IMAGE_FILE);
-        testGuess(ImageFormats.GIF, GIF_IMAGE_FILE);
-        testGuess(ImageFormats.ICNS, ICNS_IMAGE_FILE);
-        // TODO(cmchen): add ability to sniff ICOs if possible.
-        // testGuess(ImageFormat.IMAGE_FORMAT_ICO, ICO_IMAGE_FILE);
-        testGuess(ImageFormats.TIFF, TIFF_IMAGE_FILE);
-        testGuess(ImageFormats.JPEG, JPEG_IMAGE_FILE);
-        testGuess(ImageFormats.BMP, BMP_IMAGE_FILE);
-        testGuess(ImageFormats.PSD, PSD_IMAGE_FILE);
-        testGuess(ImageFormats.PBM, PBM_IMAGE_FILE);
-        testGuess(ImageFormats.PGM, PGM_IMAGE_FILE);
-        testGuess(ImageFormats.PPM, PPM_IMAGE_FILE);
-        // TODO(cmchen): add ability to sniff TGAs if possible.
-        // testGuess(ImageFormat.IMAGE_FORMAT_TGA, TGA_IMAGE_FILE);
-        // TODO(cmchen): Add test images for these formats.
-        // testGuess(ImageFormat.IMAGE_FORMAT_PNM, PNM_IMAGE_FILE);
-        // testGuess(ImageFormat.IMAGE_FORMAT_JBIG2, JBIG2_IMAGE_FILE);
-    }
-
     public static final String UNKNOWN_IMAGE_FILE = "info.txt";
 
-    @Test
-    public void testGuess_unknown() throws Exception {
-        testGuess(ImageFormats.UNKNOWN, UNKNOWN_IMAGE_FILE);
+    private final ImageFormats expectedFormat;
+    private final String pathToFile;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[] { ImageFormats.PNG, PNG_IMAGE_FILE },
+                new Object[] { ImageFormats.GIF, GIF_IMAGE_FILE },
+                new Object[] { ImageFormats.ICNS, ICNS_IMAGE_FILE },
+                // TODO(cmchen): add ability to sniff ICOs if possible.
+                // new Object[] { ImageFormat.IMAGE_FORMAT_ICO, ICO_IMAGE_FILE },
+                new Object[] { ImageFormats.TIFF, TIFF_IMAGE_FILE },
+                new Object[] { ImageFormats.JPEG, JPEG_IMAGE_FILE },
+                new Object[] { ImageFormats.BMP, BMP_IMAGE_FILE },
+                new Object[] { ImageFormats.PSD, PSD_IMAGE_FILE },
+                new Object[] { ImageFormats.PBM, PBM_IMAGE_FILE },
+                new Object[] { ImageFormats.PGM, PGM_IMAGE_FILE },
+                new Object[] { ImageFormats.PPM, PPM_IMAGE_FILE },
+                // TODO(cmchen): add ability to sniff TGAs if possible.
+                // new Object[] { ImageFormat.IMAGE_FORMAT_TGA, TGA_IMAGE_FILE },
+                // TODO(cmchen): Add test images for these formats.
+                // new Object[] { ImageFormat.IMAGE_FORMAT_PNM, PNM_IMAGE_FILE },
+                // new Object[] { ImageFormat.IMAGE_FORMAT_JBIG2, JBIG2_IMAGE_FILE },
+                new Object[] { ImageFormats.UNKNOWN, UNKNOWN_IMAGE_FILE }
+        );
     }
 
-    public void testGuess(final ImageFormat expectedFormat, String imagePath)
-            throws Exception {
-        imagePath = FilenameUtils.separatorsToSystem(imagePath);
+    public ImagingGuessFormatTest(ImageFormats expectedFormat, String pathToFile) {
+        this.expectedFormat = expectedFormat;
+        this.pathToFile = pathToFile;
+    }
+
+    @Test
+    public void testGuessFormat() throws Exception {
+        String imagePath = FilenameUtils.separatorsToSystem(pathToFile);
         final File imageFile = new File(TEST_IMAGE_FOLDER, imagePath);
 
-        assertTrue(imageFile.exists());
-        assertTrue(imageFile.isFile());
         final ImageFormat guessedFormat = Imaging.guessFormat(imageFile);
-        assertNotNull(guessedFormat);
-        assertEquals(guessedFormat, expectedFormat);
+        assertEquals(expectedFormat, guessedFormat);
     }
+
 }
