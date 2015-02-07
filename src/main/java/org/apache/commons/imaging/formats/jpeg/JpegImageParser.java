4a1bf67e030f949c6470a3897e16a7b47e9d424b
Michael Groß <mgmechanics@mgmechanics.de>
2015-02-07 13:15:01 +0100

    added missing @throws tags to javadoc

--- src/main/java/org/apache/commons/imaging/formats/jpeg/JpegImageParser.java
+++ src/main/java/org/apache/commons/imaging/formats/jpeg/JpegImageParser.java
@@ -525,6 +525,8 @@ public class JpegImageParser extends ImageParser {
      * @param params
      *            Map of optional parameters, defined in ImagingConstants.
      * @return Xmp Xml as String, if present. Otherwise, returns null.
+     * @throws org.apache.commons.imaging.ImageReadException
+     * @throws java.io.IOException
      */
     @Override
     public String getXmpXml(final ByteSource byteSource, final ImagingParameters params)
