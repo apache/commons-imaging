f8b45e3ad37d84cd020c30b0cc77f1ebbcbc2fdc
Michael Groß <mgmechanics@mgmechanics.de>
2015-02-07 10:31:02 +0100

    added missing @throws tags to javadoc

--- src/main/java/org/apache/commons/imaging/formats/gif/GifImageParser.java
+++ src/main/java/org/apache/commons/imaging/formats/gif/GifImageParser.java
@@ -994,6 +994,8 @@ public class GifImageParser extends ImageParser {
      * @param params
      *            Map of optional parameters, defined in ImagingConstants.
      * @return Xmp Xml as String, if present. Otherwise, returns null.
+     * @throws org.apache.commons.imaging.ImageReadException
+     * @throws java.io.IOException
      */
     @Override
     public String getXmpXml(final ByteSource byteSource, final ImagingParameters params)
