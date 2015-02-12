c42bcc9d9589ce94532d663fee75af1442619b39
Michael Groß <mgmechanics@mgmechanics.de>
2015-02-12 20:46:16 +0100

    Replaced parameter Map<String, Object> by ImagingParameters

--- src/test/java/org/apache/commons/imaging/examples/ImageWriteExample.java
+++ src/test/java/org/apache/commons/imaging/examples/ImageWriteExample.java
@@ -13,21 +13,21 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changed 2015 by Michael Gross, mgmechanics@mgmechanics.de
  */
 package org.apache.commons.imaging.examples;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.imaging.ImageFormat;
 import org.apache.commons.imaging.ImageFormats;
 import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.imaging.ImageWriteException;
 import org.apache.commons.imaging.Imaging;
-import org.apache.commons.imaging.ImagingConstants;
+import org.apache.commons.imaging.ImagingParametersTiff;
 import org.apache.commons.imaging.formats.tiff.constants.TiffConstants;
 
 public class ImageWriteExample {
@@ -37,11 +37,10 @@ public class ImageWriteExample {
         final BufferedImage image = Imaging.getBufferedImage(file);
 
         final ImageFormat format = ImageFormats.TIFF;
-        final Map<String, Object> params = new HashMap<String, Object>();
+        final ImagingParametersTiff params = new ImagingParametersTiff();
 
         // set optional parameters if you like
-        params.put(ImagingConstants.PARAM_KEY_COMPRESSION, new Integer(
-                TiffConstants.TIFF_COMPRESSION_UNCOMPRESSED));
+        params.setCompressionLevel(TiffConstants.TIFF_COMPRESSION_UNCOMPRESSED);
 
         final byte[] bytes = Imaging.writeImageToBytes(image, format, params);
 
