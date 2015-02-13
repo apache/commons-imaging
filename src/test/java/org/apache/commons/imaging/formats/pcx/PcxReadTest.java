bbb966b3f2cc7f1744b5e86ff675682d6c584c8e
Michael Groß <mgmechanics@mgmechanics.de>
2015-02-13 18:36:10 +0100

    Replaced parameter Map<String, Object> by ImagingParameters

--- src/test/java/org/apache/commons/imaging/formats/pcx/PcxReadTest.java
+++ src/test/java/org/apache/commons/imaging/formats/pcx/PcxReadTest.java
@@ -13,6 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changed 2015 by Michael Gross, mgmechanics@mgmechanics.de
  */
 
 package org.apache.commons.imaging.formats.pcx;
@@ -21,12 +23,11 @@ import static org.junit.Assert.assertNotNull;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.imaging.ImageInfo;
 import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.ImagingParameters;
 import org.apache.commons.imaging.common.ImageMetadata;
 import org.apache.commons.imaging.util.Debug;
 import org.junit.Assert;
@@ -47,7 +48,7 @@ public class PcxReadTest extends PcxBaseTest {
             final ImageMetadata metadata = Imaging.getMetadata(imageFile);
             Assert.assertFalse(metadata instanceof File); // Dummy check to avoid unused warning (it may be null)
 
-            final Map<String, Object> params = new HashMap<String, Object>();
+            final ImagingParameters params = new ImagingParameters();
             final ImageInfo imageInfo = Imaging.getImageInfo(imageFile, params);
             assertNotNull(imageInfo);
 
