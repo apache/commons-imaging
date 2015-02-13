8ec13e8d1a5f3001e09e37b7d21190f2b7acfcea
Michael Groß <mgmechanics@mgmechanics.de>
2015-02-13 18:15:43 +0100

    made field final to get told if I broke something

--- src/test/java/org/apache/commons/imaging/formats/jpeg/xmp/JpegXmpRewriteTest.java
+++ src/test/java/org/apache/commons/imaging/formats/jpeg/xmp/JpegXmpRewriteTest.java
@@ -41,7 +41,7 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class JpegXmpRewriteTest extends JpegXmpBaseTest {
 
-    private File imageFile;
+    private final File imageFile;
 
     @Parameterized.Parameters
     public static Collection<File> data() throws Exception {
