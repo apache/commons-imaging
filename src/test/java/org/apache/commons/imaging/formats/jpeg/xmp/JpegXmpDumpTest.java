58a3155ba62f81fe706105569f557c7a44153dbb
Michael Groß <mgmechanics@mgmechanics.de>
2015-02-13 18:16:51 +0100

    Replaced parameter Map<String, Object> by ImagingParameters

--- src/test/java/org/apache/commons/imaging/formats/jpeg/xmp/JpegXmpDumpTest.java
+++ src/test/java/org/apache/commons/imaging/formats/jpeg/xmp/JpegXmpDumpTest.java
@@ -13,6 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changed 2015 by Michael Gross, mgmechanics@mgmechanics.de
  */
 
 package org.apache.commons.imaging.formats.jpeg.xmp;
@@ -21,8 +23,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.imaging.ImagingParameters;
 
 import org.apache.commons.imaging.common.bytesource.ByteSource;
 import org.apache.commons.imaging.common.bytesource.ByteSourceFile;
@@ -48,7 +49,7 @@ public class JpegXmpDumpTest extends JpegXmpBaseTest {
     @Test
     public void test() throws Exception {
         final ByteSource byteSource = new ByteSourceFile(imageFile);
-        final Map<String, Object> params = new HashMap<String, Object>();
+        final ImagingParameters params = new ImagingParameters();
         final String xmpXml = new JpegImageParser().getXmpXml(byteSource, params);
 
         // TODO assert more
