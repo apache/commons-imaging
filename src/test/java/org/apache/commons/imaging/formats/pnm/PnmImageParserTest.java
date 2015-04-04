0cc9fbb94b05d2f3aaecbb381178ac7ddde2c0c3
Nebu Pookins <nebu@nebupookins.net>
2015-04-04 08:30:45 -0700

    Fix header parse bug in PnmImageParser.
    Prior to this commit, PnmImagePArser would throw
    NumberFormatException if the width or header could not be parsed.
    Now it throws ImageReadException to be more consisten with the
    rest of the commons-imaging API.

---
+++ src/test/java/org/apache/commons/imaging/formats/pnm/PnmImageParserTest.java
@@ -0,0 +1,47 @@
+package org.apache.commons.imaging.formats.pnm;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.commons.imaging.ImageInfo;
+import org.apache.commons.imaging.ImageReadException;
+import org.junit.Test;
+
+public class PnmImageParserTest {
+
+  @Test
+  public void testGetImageInfo_happyCase() throws ImageReadException, IOException {
+    byte[] bytes = "P1\n3 2\n0 1 0\n1 0 1\n".getBytes(StandardCharsets.US_ASCII);
+    Map<String, Object> params = Collections.emptyMap();
+    PnmImageParser underTest = new PnmImageParser();
+    ImageInfo results = underTest.getImageInfo(bytes, params);
+    assertEquals(results.getBitsPerPixel(), 1);
+    assertEquals(results.getWidth(), 3);
+    assertEquals(results.getHeight(), 2);
+    assertEquals(results.getNumberOfImages(), 1);
+  }
+
+  /**
+   * If an invalid width is specified, should throw {@link ImageReadException} rather than
+   * {@link NumberFormatException}.
+   */
+  @Test(expected = ImageReadException.class)
+  public void testGetImageInfo_invalidWidth() throws ImageReadException, IOException {
+    byte[] bytes = "P1\na 2\n0 0 0 0 0 0 0 0 0 0 0\n1 1 1 1 1 1 1 1 1 1 1\n".getBytes(StandardCharsets.US_ASCII);
+    Map<String, Object> params = Collections.emptyMap();
+    PnmImageParser underTest = new PnmImageParser();
+    underTest.getImageInfo(bytes, params);
+  }
+
+  @Test(expected = ImageReadException.class)
+  public void testGetImageInfo_invalidHeight() throws ImageReadException, IOException {
+    byte[] bytes = "P1\n2 a\n0 0\n0 0\n0 0\n0 0\n0 0\n0 1\n1 1\n1 1\n1 1\n1 1\n1 1\n".getBytes(StandardCharsets.US_ASCII);
+    Map<String, Object> params = Collections.emptyMap();
+    PnmImageParser underTest = new PnmImageParser();
+    underTest.getImageInfo(bytes, params);
+  }
+}
