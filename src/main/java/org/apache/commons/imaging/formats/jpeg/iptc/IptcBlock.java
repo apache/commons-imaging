3653791bb2ec9e9224beee7da3525af2cf866f32
Sebastian Bazley <sebb@apache.org>
2016-10-11 15:50:18 +0000

    Oops - the class is used in public parameter types
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/imaging/trunk@1764274 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcBlock.java
+++ src/main/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcBlock.java
@@ -19,19 +19,21 @@ package org.apache.commons.imaging.formats.jpeg.iptc;
 /**
  * Represents an IPTC block, a set of key-value pairs of Photoshop IPTC data.
  */
-class IptcBlock {
+public class IptcBlock {
+    // N.B. The class is used in public API parameter types
+
     public final int blockType;
     //  Only currently used by classes in the package
     final byte[] blockNameBytes; // TODO make private and provide getter?
     final byte[] blockData; // TODO make private and provide getter?
 
-    IptcBlock(final int blockType, final byte[] blockNameBytes, final byte[] blockData) {
+    public IptcBlock(final int blockType, final byte[] blockNameBytes, final byte[] blockData) {
         this.blockData = blockData;
         this.blockNameBytes = blockNameBytes;
         this.blockType = blockType;
     }
 
-    boolean isIPTCBlock() {
+    public boolean isIPTCBlock() {
         return blockType == IptcConstants.IMAGE_RESOURCE_BLOCK_IPTC_DATA;
     }
 }
