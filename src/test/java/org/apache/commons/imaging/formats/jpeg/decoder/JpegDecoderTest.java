6a79d35d6654d895d0a4b73b3a9282ec9aaeeb06
Bruno P. Kinoshita <kinow@apache.org>
2019-01-02 21:00:32 +1300

    IMAGING-220: replace while by if in the JPEG decoder extend method

---
+++ src/test/java/org/apache/commons/imaging/formats/jpeg/decoder/JpegDecoderTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.imaging.formats.jpeg.decoder;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.common.bytesource.ByteSourceFile;
+import org.junit.Test;
+
+/**
+ * Tests for the JpegDecoder.
+ */
+public class JpegDecoderTest {
+
+    /**
+     * Test that a bad file does not hang or take too long to be processed.
+     *
+     * @throws IOException
+     * @throws ImageReadException
+     */
+    @Test(expected = ImageReadException.class, timeout = 2000)
+    public void testDecodeBadFile() throws ImageReadException, IOException {
+        // From IMAGING-220
+        File inputFile = new File(
+                JpegDecoderTest.class.getResource("/IMAGING-220/timeout-48eb4251935b4ca8b26d1859ea525c1b42ae0c78")
+                        .getFile());
+        ByteSourceFile byteSourceFile = new ByteSourceFile(inputFile);
+        new JpegDecoder().decode(byteSourceFile);
+    }
+}
