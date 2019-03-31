115893bcdfea69fc628d3b990731799df9631a05
Gary Gregory <gardgregory@gmail.com>
2019-03-31 09:50:55 -0400

    Remove trailing spaces.

--- src/test/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcFullDiscardTest.java
+++ src/test/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcFullDiscardTest.java
@@ -28,7 +28,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class IptcFullDiscardTest {
-    
+
     private byte[] addMetaData(final byte[] bytes) throws Exception {
         final IptcRecord record = new IptcRecord(IptcTypes.KEYWORDS, "meta; data");
         final PhotoshopApp13Data data = new PhotoshopApp13Data(Collections.singletonList(record), Collections.<IptcBlock> emptyList());
@@ -36,7 +36,7 @@ public class IptcFullDiscardTest {
         new JpegIptcRewriter().writeIPTC(bytes, byteArrayOutputStream, data);
         return byteArrayOutputStream.toByteArray();
     }
-    
+
     private byte[] generateImage() throws Exception {
         final BufferedImage image = new BufferedImage(100, 50, BufferedImage.TYPE_3BYTE_BGR); // was TYPE_INT_ARGB but that fails on Continuum
         final Graphics2D graphics2D = image.createGraphics();
@@ -45,7 +45,7 @@ public class IptcFullDiscardTest {
         ImageIO.write(image, "jpg", byteArrayOutputStream);
         return byteArrayOutputStream.toByteArray();
     }
-    
+
     private byte[] removeMetaData(final byte[] bytes, final boolean removeApp13Segment) throws Exception {
         final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         new JpegIptcRewriter().removeIPTC(bytes, byteArrayOutputStream, removeApp13Segment);
@@ -59,7 +59,7 @@ public class IptcFullDiscardTest {
         final byte[] untaggedImage = removeMetaData(taggedImage, false);
         Assert.assertEquals(18, untaggedImage.length - originalImage.length);
     }
-    
+
     @Test
     public void removeApp13Segment() throws Exception {
         final byte[] originalImage = generateImage();
