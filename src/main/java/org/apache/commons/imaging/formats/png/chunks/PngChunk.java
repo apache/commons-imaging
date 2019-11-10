003613430e9a8e60a67ed033d8f06ca0b09a36c3
Bruno P. Kinoshita <kinow@apache.org>
2019-11-10 20:01:25 +1300

    [IMAGING-238]: Add javadocs for the PngChunk class

--- src/main/java/org/apache/commons/imaging/formats/png/chunks/PngChunk.java
+++ src/main/java/org/apache/commons/imaging/formats/png/chunks/PngChunk.java
@@ -20,6 +20,12 @@ import java.io.ByteArrayInputStream;
 
 import org.apache.commons.imaging.common.BinaryFileParser;
 
+/**
+ * A PNG image is composed of several chunks. This is the base class for the chunks,
+ * used by the parser.
+ *
+ * @see https://en.wikipedia.org/wiki/Portable_Network_Graphics#%22Chunks%22_within_the_file
+ */
 public class PngChunk extends BinaryFileParser {
     public final int length;
     public final int chunkType;
@@ -32,6 +38,13 @@ public class PngChunk extends BinaryFileParser {
     public final boolean reserved;
     public final boolean safeToCopy;
 
+    /**
+     * Constructor.
+     * @param length chunk length
+     * @param chunkType chunk type
+     * @param crc CRC computed over the chunk type and chunk data (but not the length)
+     * @param bytes chunk data bytes
+     */
     public PngChunk(final int length, final int chunkType, final int crc, final byte[] bytes) {
         this.length = length;
         this.chunkType = chunkType;
@@ -53,14 +66,29 @@ public class PngChunk extends BinaryFileParser {
         safeToCopy = propertyBits[3];
     }
 
+    /**
+     * Return a copy of the chunk bytes.
+     * @return the chunk bytes
+     */
     public byte[] getBytes() {
         return bytes.clone();
     }
 
+    /**
+     * Return a copy of the chunk property bits.
+     * @return the chunk property bits
+     */
     public boolean[] getPropertyBits() {
         return propertyBits.clone();
     }
 
+    /**
+     * Create and return a {@link ByteArrayInputStream} for the chunk bytes.
+     *
+     * <p>The caller is responsible for closing the resource.</p>
+     *
+     * @return a ByteArrayInputStream for the chunk bytes
+     */
     protected ByteArrayInputStream getDataStream() {
         return new ByteArrayInputStream(getBytes());
     }
