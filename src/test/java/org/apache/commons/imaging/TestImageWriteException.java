af4bf94319c7dc09567551e84363d63ae32803a0
Allon Mureinik <allon@synopsys.com>
2019-11-24 09:01:56 +0200

    IMAGING-242 Standardize TestImageWriteException asserts
    Use assertion methods from org.junit.jupiter.api.Assertions instead
    of org.junit.Assert, as per the project's standard.

--- src/test/java/org/apache/commons/imaging/TestImageWriteException.java
+++ src/test/java/org/apache/commons/imaging/TestImageWriteException.java
@@ -16,9 +16,9 @@
  */
 package org.apache.commons.imaging;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.stream.Stream;
 
