d53cdd94deb1fb09619b5189ec19ed2636f2bd1c
Bruno P. Kinoshita <kinow@apache.org>
2020-04-10 16:11:45 +1200

    [IMAGING-253]: Re-format file (4 spaces instead of 2)

--- src/test/java/org/apache/commons/imaging/common/bytesource/ByteSourceInputStreamTest.java
+++ src/test/java/org/apache/commons/imaging/common/bytesource/ByteSourceInputStreamTest.java
@@ -33,26 +33,24 @@ import java.util.Collections;
 
 class ByteSourceInputStreamTest {
 
-  public static final String ICO_IMAGE_FILE = "ico\\1\\Oregon Scientific DS6639 - DSC_0307 - small.ico";
-  public static final int ICO_IMAGE_WIDTH = 300;
-  public static final int ICO_IMAGE_HEIGHT = 225;
+    public static final String ICO_IMAGE_FILE = "ico\\1\\Oregon Scientific DS6639 - DSC_0307 - small.ico";
+    public static final int ICO_IMAGE_WIDTH = 300;
+    public static final int ICO_IMAGE_HEIGHT = 225;
 
-  @Test
-  public void testReadFromStream() throws IOException, ImageReadException {
+    @Test
+    public void testReadFromStream() throws IOException, ImageReadException {
 
-    final String imagePath = FilenameUtils.separatorsToSystem(ICO_IMAGE_FILE);
-    final File imageFile = new File(ImagingTestConstants.TEST_IMAGE_FOLDER, imagePath);
-    BufferedInputStream imageStream = new BufferedInputStream(new FileInputStream(imageFile));
+        final String imagePath = FilenameUtils.separatorsToSystem(ICO_IMAGE_FILE);
+        final File imageFile = new File(ImagingTestConstants.TEST_IMAGE_FOLDER, imagePath);
+        BufferedInputStream imageStream = new BufferedInputStream(new FileInputStream(imageFile));
 
-    // ByteSourceInputStream is created inside of following method
-    BufferedImage bufferedImage = Imaging.getBufferedImage(
-        imageStream,
-        Collections.singletonMap(ImagingConstants.PARAM_KEY_FILENAME, ICO_IMAGE_FILE)
-    );
+        // ByteSourceInputStream is created inside of following method
+        BufferedImage bufferedImage = Imaging.getBufferedImage(imageStream,
+                Collections.singletonMap(ImagingConstants.PARAM_KEY_FILENAME, ICO_IMAGE_FILE));
 
-    Assertions.assertEquals(bufferedImage.getWidth(), ICO_IMAGE_WIDTH);
-    Assertions.assertEquals(bufferedImage.getHeight(), ICO_IMAGE_HEIGHT);
+        Assertions.assertEquals(bufferedImage.getWidth(), ICO_IMAGE_WIDTH);
+        Assertions.assertEquals(bufferedImage.getHeight(), ICO_IMAGE_HEIGHT);
 
-  }
+    }
 
 }
