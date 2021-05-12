41940939e5f6479708e6c9d52b22874af4d6fc09
Arturo Bernal <arturobernalg@gmail.com>
2021-05-12 22:16:23 +0200

    [IMAGING-304] Use Comparator combinator

--- src/main/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcRecord.java
+++ src/main/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcRecord.java
@@ -24,7 +24,7 @@ import java.util.Comparator;
 public class IptcRecord {
     public final IptcType iptcType;
     private final String value;
-    public static final Comparator<IptcRecord> COMPARATOR = (e1, e2) -> e1.iptcType.getType() - e2.iptcType.getType();
+    public static final Comparator<IptcRecord> COMPARATOR = Comparator.comparingInt(e -> e.iptcType.getType());
 
     public IptcRecord(final IptcType iptcType, final String value) {
         this.iptcType = iptcType;
