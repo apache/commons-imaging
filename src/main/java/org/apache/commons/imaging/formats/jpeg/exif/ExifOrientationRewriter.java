fb093087cee3e81a09a05f0009156906c9be1a5a
snumlautoken <arami@live.se>
2023-02-28 16:58:07 +0100

    (feature) #34 Add output methods

--- src/main/java/org/apache/commons/imaging/formats/jpeg/exif/ExifOrientationRewriter.java
+++ src/main/java/org/apache/commons/imaging/formats/jpeg/exif/ExifOrientationRewriter.java
@@ -18,7 +18,8 @@
 package org.apache.commons.imaging.formats.jpeg.exif;
 
 import java.io.File;
-import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.apache.commons.imaging.common.bytesource.ByteSource;
 import org.apache.commons.imaging.common.bytesource.ByteSourceArray;
@@ -60,12 +61,42 @@ public class ExifOrientationRewriter {
         fileSrc = byteSource;
     }
 
-    public Orientation GetExifOrientation() {
+    public Orientation getExifOrientation() {
         return Orientation.HORIZONTAL;
     }
 
-    public void SetExifOrientation(Orientation orientation) {
+    public void setExifOrientation(Orientation orientation) {
 
+    }
+
+    /**
+     * @return the ByteSource of the current file
+     */
+    public ByteSource getOutput() {
+        return fileSrc;
+    }
+
+    /**
+     * Assigns the ByteSource of the current file in the form of a byte array 
+     * @param byteArrayIn byteArray which is assigned the source of the file
+     * @throws IOException
+     */
+    public void getOutput(byte[] byteArrayIn) 
+        throws IOException {
+        byteArrayIn = fileSrc.getAll();
+    }
+
+    /**
+     *  Writes Bytesource to file with given path
+     * @param path String of the path in which the file is written
+     * @throws IOException
+     */
+    public void getOutput(String path) 
+        throws IOException {
+        final File tempFile =  new File(path);
+        try (FileOutputStream outputStream = new FileOutputStream(tempFile)) {
+            outputStream.write(fileSrc.getAll());
+        }
     }
 
 }
