001c3eec1ec1aded78fff7ebf8bf89b6ebb14549
snumlautoken <arami@live.se>
2023-03-01 19:00:14 +0100

    (feature) #37 #38 Add getter tests

--- src/test/java/org/apache/commons/imaging/formats/jpeg/exif/ExifOrientationRewriterTest.java
+++ src/test/java/org/apache/commons/imaging/formats/jpeg/exif/ExifOrientationRewriterTest.java
@@ -17,10 +17,119 @@
 
 
 package org.apache.commons.imaging.formats.jpeg.exif;
-import org.apache.commons.imaging.formats.jpeg.exif.ExifOrientationRewriter;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.ImageWriteException;
+import org.apache.commons.imaging.common.bytesource.ByteSource;
+import org.apache.commons.imaging.common.bytesource.ByteSourceArray;
+import org.apache.commons.imaging.common.bytesource.ByteSourceFile;
+import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
+import org.apache.commons.imaging.formats.jpeg.exif.ExifOrientationRewriter.Orientation;
+import org.apache.commons.imaging.formats.tiff.TiffImageMetadata;
+import org.apache.commons.imaging.formats.tiff.constants.TiffTagConstants;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputSet;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
 public class ExifOrientationRewriterTest extends ExifBaseTest {
-    
+
+    @Test
+    public void originalOrientationMatchesRewriterGet() 
+        throws ImageReadException, IOException, ImageWriteException {
+
+        final List<File> images = getImagesWithExifData();
+        for (final File imageFile : images) {            
+            final JpegImageMetadata originalMetadata = (JpegImageMetadata) Imaging.getMetadata(imageFile);
+            assertNotNull(originalMetadata);
+
+            final TiffImageMetadata originalExifMetadata = originalMetadata.getExif();
+            assertNotNull(originalExifMetadata);
+
+            short originalOrt = (short) originalExifMetadata.getFieldValue(TiffTagConstants.TIFF_TAG_ORIENTATION);
+
+            ExifOrientationRewriter rewriter = new ExifOrientationRewriter(imageFile);
+            short ort = rewriter.getExifOrientation().getVal();
+
+            assertEquals(ort, originalOrt);
+        }
+
+    }
+
+    @Test
+    public void rewrittenOrientationMatchesRewriterGet()
+        throws ImageReadException, IOException, ImageWriteException {
+        
+        final List<File> images = getImagesWithExifData();
+        for (final File imageFile : images) {            
+            final ByteSource byteSource = new ByteSourceFile(imageFile);
+
+            final JpegImageMetadata originalMetadata = (JpegImageMetadata) Imaging.getMetadata(imageFile);
+            assertNotNull(originalMetadata);
+
+            final TiffImageMetadata originalExifMetadata = originalMetadata.getExif();
+            assertNotNull(originalExifMetadata);
+
+            final TiffOutputSet outputSet = originalExifMetadata.getOutputSet();
+
+            outputSet.getOrCreateRootDirectory().removeField(TiffTagConstants.TIFF_TAG_ORIENTATION);
+            outputSet.getOrCreateRootDirectory().add(TiffTagConstants.TIFF_TAG_ORIENTATION, (short) TiffTagConstants.ORIENTATION_VALUE_ROTATE_270_CW);
+
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            new ExifRewriter().updateExifMetadataLossy(byteSource.getAll(), baos,
+                    outputSet);
+
+            final byte[] bytes = baos.toByteArray();
+            final File tempFile = Files.createTempFile("inserted" + "_", ".jpg").toFile();
+
+            FileUtils.writeByteArrayToFile(tempFile, bytes);
+
+            final ExifOrientationRewriter rewriter = new ExifOrientationRewriter(tempFile);
+            short ort = rewriter.getExifOrientation().getVal();
+
+            assertEquals(ort, (short) TiffTagConstants.ORIENTATION_VALUE_ROTATE_270_CW);
+        }    
+
+    }
+
+    @Test
+    public void getWithNullExifReturnsHorizontal() 
+        throws ImageReadException, IOException, ImageWriteException {
+
+        final List<File> images = getImagesWithExifData();
+        for (final File imageFile : images) {            
+            final ByteSource byteSource = new ByteSourceFile(imageFile);
+            final JpegImageMetadata originalMetadata = (JpegImageMetadata) Imaging.getMetadata(imageFile);
+            assertNotNull(originalMetadata);
+
+            final TiffImageMetadata originalExifMetadata = originalMetadata.getExif();
+            assertNotNull(originalExifMetadata);
+
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            new ExifRewriter().removeExifMetadata(byteSource, baos);
+            final byte[] bytes = baos.toByteArray();
+            final File tempFile = Files.createTempFile("removed", ".jpg").toFile();
+            FileUtils.writeByteArrayToFile(tempFile, bytes);
+
+            assertFalse(hasExifData(tempFile));
+
+            final ExifOrientationRewriter rewriter = new ExifOrientationRewriter(tempFile);
+            short ort = rewriter.getExifOrientation().getVal();
+
+            assertEquals(ort, Orientation.HORIZONTAL.getVal());
+        }
+
+    }
+
+
 }
