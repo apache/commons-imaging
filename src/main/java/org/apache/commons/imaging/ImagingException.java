c35d650951a071fe9ddfc1e7789904294b6d0222
Gary Gregory <garydgregory@gmail.com>
2023-05-14 10:55:26 -0400

    Javadoc: Fix unsupported tag on Java 11

--- src/main/java/org/apache/commons/imaging/ImagingException.java
+++ src/main/java/org/apache/commons/imaging/ImagingException.java
@@ -42,7 +42,7 @@ public class ImagingException extends Exception {
      * </p>
      *
      * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). (A <tt>null</tt>
+     * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null}
      *                value is permitted, and indicates that the cause is nonexistent or unknown.)
      */
     public ImagingException(final String message, final Throwable cause) {
