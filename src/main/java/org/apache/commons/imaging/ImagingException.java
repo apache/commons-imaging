70daf9b3b62b036ac0206fe71f74149ab251e52e
Gary Gregory <garydgregory@gmail.com>
2023-05-14 09:37:11 -0400

    Javadoc

--- src/main/java/org/apache/commons/imaging/ImagingException.java
+++ src/main/java/org/apache/commons/imaging/ImagingException.java
@@ -17,16 +17,34 @@
 package org.apache.commons.imaging;
 
 /**
- * The base class for implementing custom exceptions in the
- * Apache Commons Imaging package.
+ * The root class for implementing custom exceptions in the Apache Commons Imaging component.
  */
 public class ImagingException extends Exception {
+
     private static final long serialVersionUID = -1L;
 
+    /**
+     * Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently
+     * be initialized by a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for later retrieval by the {@link #getMessage()}
+     *                method.
+     */
     public ImagingException(final String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     * <p>
+     * Note that the detail message associated with {@code cause} is <em>not</em> automatically incorporated in this
+     * exception's detail message.
+     * </p>
+     *
+     * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). (A <tt>null</tt>
+     *                value is permitted, and indicates that the cause is nonexistent or unknown.)
+     */
     public ImagingException(final String message, final Throwable cause) {
         super(message, cause);
     }
