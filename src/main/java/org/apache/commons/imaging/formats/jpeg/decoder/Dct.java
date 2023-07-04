c8a200645a299af1688226954cd09b6c5301c8e3
Gary Gregory <garydgregory@gmail.com>
2023-07-04 08:05:19 -0400

    Update URLs in comments
    - Document broken URLs
    - Use HTTPS instead of HTTP in comments where available
    - Format tweaks

--- src/main/java/org/apache/commons/imaging/formats/jpeg/decoder/Dct.java
+++ src/main/java/org/apache/commons/imaging/formats/jpeg/decoder/Dct.java
@@ -219,7 +219,7 @@ final class Dct {
 
     /**
      * Fast inverse Dct using AA&N. This is taken from the beautiful
-     * http://vsr.finermatik.tu-chemnitz.de/~jan/MPEG/HTML/IDCT.html which gives
+     * [BROEKN URL] http://vsr.finermatik.tu-chemnitz.de/~jan/MPEG/HTML/IDCT.html which gives
      * easy equations and properly explains constants and scaling factors. Terms
      * have been inlined and the negation optimized out of existence.
      */
