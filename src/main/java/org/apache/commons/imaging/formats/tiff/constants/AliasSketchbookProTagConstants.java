81a1e15183e84911741f20ad51fda06b5f916bde
Gary Gregory <garydgregory@gmail.com>
2023-07-04 08:04:30 -0400

    Update URLs in comments
    - Document broken URLs
    - Use HTTPS instead of HTTP in comments where available
    - Format tweaks

--- src/main/java/org/apache/commons/imaging/formats/tiff/constants/AliasSketchbookProTagConstants.java
+++ src/main/java/org/apache/commons/imaging/formats/tiff/constants/AliasSketchbookProTagConstants.java
@@ -26,7 +26,7 @@ import org.apache.commons.imaging.formats.tiff.taginfos.TagInfoAscii;
 /**
  * Alias Sketchbook Pro multi-layer TIFF.
  * <p>
- * http://www.awaresystems.be/imaging/tiff/tifftags/docs/alias.html
+ * https://www.awaresystems.be/imaging/tiff/tifftags/docs/alias.html
  * </p>
  */
 public final class AliasSketchbookProTagConstants {
