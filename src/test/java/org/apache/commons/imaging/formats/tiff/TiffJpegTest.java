fd7e23527830b3469f9c1b8fb567694b8da5838d
gwlucastrig <contact.tinfour@gmail.com>
2023-11-18 12:01:15 -0500

    [Imaging-369] Review comments

--- src/test/java/org/apache/commons/imaging/formats/tiff/TiffJpegTest.java
+++ src/test/java/org/apache/commons/imaging/formats/tiff/TiffJpegTest.java
@@ -38,22 +38,22 @@ public class TiffJpegTest extends TiffBaseTest {
         "TestJpegTiles.tiff",
         "TestJpegStripsRgb.tiff",
         "TestJpegTilesRgb.tiff",
-        "TestJpegCMYK.tiff",};
+        "TestJpegCMYK.tiff"};
 
     static final String[] testSet1 = {
         "TestSample264x264.tiff",
         "TestJpegStrips264x264.tiff",
         "TestJpegStrips264x264BigEndian.tiff",
         "TestJpegTiles264x264.tiff",
-        "TestJpegTiles264x264BigEndian.tiff",};
+        "TestJpegTiles264x264BigEndian.tiff"};
 
     static final String[] testSet2 = {
         "TestSampleArgb.tiff",
-        "TestJpegArgb.tiff",};
+        "TestJpegArgb.tiff"};
 
       static final String[] testSet3 = {
         "TestSampleRgb127x127.tiff",
-        "TestJpegRgb127x127.tiff",};
+        "TestJpegRgb127x127.tiff"};
 
 
     private File getTiffFile(String name) {
@@ -170,7 +170,7 @@ public class TiffJpegTest extends TiffBaseTest {
         // test set 3 includes an add-valued width and height that does
         // not evently fit a block of JPEG data. It is used to test
         // for the proper handling of edge-cases.
-         processTestSet(testSet3);
+        processTestSet(testSet3);
     }
 
     /**
