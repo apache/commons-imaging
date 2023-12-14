d4af2e00167af050fa0db022662160b4c46faafb
Franco N. Merenda <merenda.franco83@gmail.com>
2023-12-14 18:51:25 +0100

    Add Java Microbenchmarking Tool Dependency (#8)
    * Added idea files to gitignore
    
    * Added microbenchmark dependency and base runner
    
    * Excluding from rat report
    
    * Added missing exclusion for benchmark runner

---
+++ src/test/java/org/apache/commons/imaging/BenchmarkRunner.java
@@ -0,0 +1,7 @@
+package org.apache.commons.imaging;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
