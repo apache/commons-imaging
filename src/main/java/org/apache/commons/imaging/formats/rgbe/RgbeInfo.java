d0deeff896346e1c0b8adfa150fdf56dbc44c70c
Julian Orcinoli <orcinoli@gmail.com>
2023-12-14 17:09:07 +0100

    casting to long

--- src/main/java/org/apache/commons/imaging/formats/rgbe/RgbeInfo.java
+++ src/main/java/org/apache/commons/imaging/formats/rgbe/RgbeInfo.java
@@ -127,7 +127,7 @@ class RgbeInfo implements Closeable {
                     if (0 == mantissa) {
                         out[channel][pos] = 0;
                     } else {
-                        final float mult = (float) Math.pow(2, mantissa - (128 + 8));
+                        final float mult = (float) Math.pow(2, (double)mantissa - (128 + 8));
                         out[channel][pos] = ((rgbe[p + channelOffset] & 0xff) + 0.5f) * mult;
                     }
                 }
