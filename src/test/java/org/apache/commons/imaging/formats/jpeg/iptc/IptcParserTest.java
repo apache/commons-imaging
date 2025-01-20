29b0b0a7383f949ccc3aa415717ea701b544cd19
Thomas Stieler <stieler@subshell.com>
2025-01-20 17:26:50 +0100

    Add proposal for unit test

--- src/test/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcParserTest.java
+++ src/test/java/org/apache/commons/imaging/formats/jpeg/iptc/IptcParserTest.java
@@ -21,20 +21,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.commons.imaging.ImagingException;
 import org.apache.commons.imaging.bytesource.ByteSource;
+import org.apache.commons.imaging.common.BinaryOutputStream;
 import org.apache.commons.imaging.common.GenericImageMetadata.GenericImageMetadataItem;
 import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
 import org.apache.commons.imaging.formats.jpeg.JpegImageParser;
 import org.apache.commons.imaging.formats.jpeg.JpegImagingParameters;
 import org.apache.commons.imaging.formats.jpeg.JpegPhotoshopMetadata;
 import org.apache.commons.imaging.test.TestResources;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * Tests for the {#link {@link IptcParser} class.
@@ -59,6 +65,74 @@ public class IptcParserTest {
         final GenericImageMetadataItem thanksInMandarin = items.get(3);
         // converted the thank-you in chinese characters to unicode for comparison here
         assertArrayEquals("\u8c22\u8c22".getBytes(StandardCharsets.UTF_8), thanksInMandarin.getText().getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Tests the correct encoding when writing IptcRecords with method {@link IptcParser#writeIptcBlock(List, boolean)}. <br />
+     * The encoding has to be UTF-8, if either the Parameter {@code forceUtf8Encoding} is set to true or if a value from the passed {@link IptcRecord} instances
+     * cannot be represented in charset ISO-8859-1.
+     *
+     * @param value     the value to test
+     * @param forceUtf8 if UTF-8 encoding should be forced
+     *
+     */
+    @ParameterizedTest
+    @CsvSource({ "äöü ÄÖÜß, true", "äöü ÄÖÜß €, true", "äöü ÄÖÜß, false", "äöü ÄÖÜß €, false" })
+    public void testEncoding(String value, boolean forceUtf8) throws IOException {
+
+        IptcParser parser = new IptcParser();
+        List<IptcRecord> records = new ArrayList<>();
+        records.add(new IptcRecord(IptcTypes.CAPTION_ABSTRACT, value));
+        final Charset charset;
+
+        //
+        byte[] actualBytes = parser.writeIptcBlock(records, forceUtf8);
+
+        // Write prefix including (optional)
+        final byte[] prefix;
+        try (ByteArrayOutputStream envelopeRecordStream = new ByteArrayOutputStream();
+                BinaryOutputStream bos = BinaryOutputStream.create(envelopeRecordStream, parser.getByteOrder())) {
+            if (forceUtf8 || value.contains("€")) {
+                // Either using UTF-8 is forced of the value contains € (which isn't a character defined in ISO-8859-1):
+                bos.write(IptcConstants.IPTC_RECORD_TAG_MARKER);
+                bos.write(IptcConstants.IPTC_ENVELOPE_RECORD_NUMBER);
+                bos.write(90); // Constant for "Coded Character Set" record
+                final byte[] codedCharset = { '\u001B', '%', 'G' };
+                bos.write2Bytes(codedCharset.length);
+                bos.write(codedCharset);
+                charset = StandardCharsets.UTF_8;
+            } else {
+                // Use ISO-8859-1 as default charset
+                charset = StandardCharsets.ISO_8859_1;
+            }
+
+            // Write version record
+            bos.write(IptcConstants.IPTC_RECORD_TAG_MARKER);
+            bos.write(IptcConstants.IPTC_APPLICATION_2_RECORD_NUMBER);
+            bos.write(IptcTypes.RECORD_VERSION.type); // record version record
+                                                      // type.
+            bos.write2Bytes(2); // record version record size
+            bos.write2Bytes(2); // record version value
+            prefix = envelopeRecordStream.toByteArray();
+        }
+
+        final byte[] applicationRecord;
+        try (ByteArrayOutputStream applicationRecordStream = new ByteArrayOutputStream();
+                BinaryOutputStream bos = BinaryOutputStream.create(applicationRecordStream, parser.getByteOrder())) {
+            bos.write(IptcConstants.IPTC_RECORD_TAG_MARKER);
+            bos.write(IptcConstants.IPTC_APPLICATION_2_RECORD_NUMBER);
+            bos.write(IptcTypes.CAPTION_ABSTRACT.type);
+            byte[] valueBytes = value.getBytes(charset);
+            bos.write2Bytes(valueBytes.length);
+            bos.write(valueBytes);
+            applicationRecord = applicationRecordStream.toByteArray();
+        }
+
+        byte[] actualPrefix = ArrayUtils.subarray(actualBytes, 0, prefix.length);
+        byte[] actualApplicationRecord = ArrayUtils.subarray(actualBytes, prefix.length, prefix.length + applicationRecord.length);
+
+        assertArrayEquals(prefix, actualPrefix);
+        assertArrayEquals(applicationRecord, actualApplicationRecord);
     }
 
     /**
