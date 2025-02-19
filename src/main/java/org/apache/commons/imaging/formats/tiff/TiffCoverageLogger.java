d01b525d375eb3eae6337061681ca4df98bd7eaf
Oscar <oarbman@kth.se>
2025-02-19 11:33:24 +0100

    Closes #4 addes a ID to all branches

--- src/main/java/org/apache/commons/imaging/formats/tiff/TiffCoverageLogger.java
+++ src/main/java/org/apache/commons/imaging/formats/tiff/TiffCoverageLogger.java
@@ -4,18 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TiffCoverageLogger {
-    private static final Map<Integer, Boolean> branchCoverage = new HashMap<>();
+    private static final Map<Integer, Boolean> branchCoverage_run = new HashMap<>();
 
     public static void logBranch_run(int branchId) {
-        branchCoverage.put(branchId, true);
+        branchCoverage_run.put(branchId, true);
     }
 
     public static void print_run() {
         System.out.println("\n\n    For function getRasterData:");
-
-        for (int i = 1; i <= 27; i++) {
-            System.out.println("    Branch " + i + ": " + (branchCoverage.getOrDefault(i, false) ? "True" : "False"));
+        int counter = 0;
+        int total_branch = 64;
+        for (int i = 1; i <= total_branch; i++) {
+            boolean is_covered = branchCoverage_run.getOrDefault(i, false);
+            System.out.println("    Branch " + i + ": " + (is_covered ? "True" : "False"));
+            if(is_covered == false){
+                counter++;
+            }
         }
+        System.out.println("\n    total missed branches : " + counter + ", This means a coverage of : " + (1 - (double)counter / total_branch) + " %");
     }
 
 
