59fe7ef158a00772d2af8b7fb6400fe90bbc443a
Lovisa Sjöberg <lovisa.sjoberg02@gmail.com>
2025-02-20 22:37:23 +0100

    Added 7 tests for nextToken(), closes issue #11. Improvement by 31 percentage points

---
+++ src/test/java/org/apache/commons/imaging/common/BasicCParserTest.java
@@ -0,0 +1,81 @@
+package org.apache.commons.imaging.common;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.ImagingException;
+import org.apache.commons.imaging.formats.tiff.TiffCoverageLogger;
+import org.junit.jupiter.api.Test;
+
+// import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BasicCParserTest {
+    
+    @Test
+    public void testEmptyStreamReturnsNull() throws IOException, ImagingException {
+        BasicCParser p = new BasicCParser(new ByteArrayInputStream(new byte[0]));
+        String result = p.nextToken();
+        assertNull(result);
+    }
+
+    @Test
+    public void testBackSlashCase() throws IOException, ImagingException {
+        String input = "\"\\wow\"";
+        BasicCParser p = new BasicCParser(new ByteArrayInputStream(input.getBytes()));
+        assertEquals("\"\\wow\"", p.nextToken());
+    }
+
+    @Test
+    public void testQuotationWithBackslash() throws IOException, ImagingException {
+        String input = "\"\\\"\"";
+        BasicCParser p = new BasicCParser(new ByteArrayInputStream(input.getBytes()));
+        assertEquals("\"\\\"\"", p.nextToken());
+    }
+
+    @Test
+    public void testRawStringPrefixAndNewlineCausesSameException() throws IOException, ImagingException {
+        String input1 = "\"\r\"";
+        String input2 = "\"\n";
+        BasicCParser p1 = new BasicCParser(new ByteArrayInputStream(input1.getBytes()));
+        BasicCParser p2 = new BasicCParser(new ByteArrayInputStream(input2.getBytes()));
+        
+        Exception e1 = assertThrows(ImagingException.class, () -> { p1.nextToken(); });
+        Exception e2 = assertThrows(ImagingException.class, () -> { p2.nextToken(); });
+
+        assertEquals(e1.getMessage(), e2.getMessage());
+    }
+
+    @Test
+    public void testValidIdentifier() throws IOException, ImagingException {
+        String input = "_abs?2485_";
+        BasicCParser p = new BasicCParser(new ByteArrayInputStream(input.getBytes()));
+        Character expected = '_';
+        assertEquals(expected, p.nextToken().charAt(0));
+    }
+
+    @Test
+    public void testTokenStartingWithTabCarriageReturnNewlineIgnored() throws IOException, ImagingException {
+        String input1 = "\t4";
+        String input2 = "\r5";
+        String input3 = "\n6";
+        BasicCParser p1 = new BasicCParser(new ByteArrayInputStream(input1.getBytes()));
+        BasicCParser p2 = new BasicCParser(new ByteArrayInputStream(input2.getBytes()));
+        BasicCParser p3 = new BasicCParser(new ByteArrayInputStream(input3.getBytes()));
+
+        String concat = p1.nextToken() + p2.nextToken() + p3.nextToken();
+
+        assertEquals("456", concat);
+    }
+
+    @Test
+    public void testInvalidCharacterException() throws IOException, ImagingException {
+        String input = "?abs?2485_";
+        BasicCParser p = new BasicCParser(new ByteArrayInputStream(input.getBytes()));
+        Exception e = assertThrows(ImagingException.class, () -> { p.nextToken(); });
+
+        String unhandled = e.getMessage().substring(0, 9);
+        assertEquals(unhandled, "Unhandled");
+    }
+}
