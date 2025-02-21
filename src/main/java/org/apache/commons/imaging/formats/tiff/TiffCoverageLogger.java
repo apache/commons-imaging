d2be9ae9cce050e75ec295d1edb83263208fec7e
hugolw <hugolw@kth.se>
2025-02-21 08:55:07 +0100

    Error fix. Removed function call not needed. Relates to #8

--- src/main/java/org/apache/commons/imaging/formats/tiff/TiffCoverageLogger.java
+++ src/main/java/org/apache/commons/imaging/formats/tiff/TiffCoverageLogger.java
@@ -24,17 +24,18 @@ public class TiffCoverageLogger {
 
     private static final Map<Integer, Boolean> branchCoverage_nextToken = new HashMap<>();
 
-
     public static void logBranch_run(int branchId) {
         branchCoverage_run.put(branchId, true);
     }
 
     public static void logBranch_run_getImageInfo(int branchId) {
         branchCoverage_run_getImageInfo.put(branchId, true);
+    }
 
     public static void getBufferedImagelogBranch_run(int branchId) {
         getBufferedImageBranchCoverage_run.put(branchId, true);
     }
+
     public static void logBranch_nextToken(int branchId) {
         branchCoverage_nextToken.put(branchId, true);
 
@@ -51,10 +52,9 @@ public class TiffCoverageLogger {
                 counter++;
             }
         }
-        System.out.println("\n    total missed branches : " + counter + ", This means a coverage of : "
-                + (1 - (double) counter / total_branch) + " %");
 
-        System.out.println("\n    total missed branches : " + counter + ", This means a coverage of : " + (1 - (double)counter / total_branch) * 100 + " %");
+        System.out.println("\n    total missed branches : " + counter + ", This means a coverage of : "
+                + (1 - (double) counter / total_branch) * 100 + " %");
 
         System.out.println("\n\n    For function nextToken():");
         int counter1 = 0;
@@ -62,44 +62,43 @@ public class TiffCoverageLogger {
         for (int i = 0; i <= total_branch1; i++) {
             boolean is_covered = branchCoverage_nextToken.getOrDefault(i, false);
             System.out.println("    Branch " + i + ": " + (is_covered ? "True" : "False"));
-            if(is_covered == false){
+            if (is_covered == false) {
                 counter1++;
             }
         }
-        System.out.println("\n    total missed branches : " + counter1 + ", This means a coverage of : " + (1 - (double)counter1 / total_branch1) * 100 + " %");
-
+        System.out.println("\n    total missed branches : " + counter1 + ", This means a coverage of : "
+                + (1 - (double) counter1 / total_branch1) * 100 + " %");
 
         System.out.println("\n\n    For function getBufferedImagePrint:");
         int bufferedImageCounter = 0;
         int bufferedImage_total_branch = 44;
         for (int i = 1; i <= bufferedImage_total_branch; i++) {
             boolean is_covered_buffered_image = getBufferedImageBranchCoverage_run.getOrDefault(i, false);
-            System.out.println("    Branch " + i + ": " + ( is_covered_buffered_image ? "True" : "False"));
-            if(is_covered_buffered_image == false){
+            System.out.println("    Branch " + i + ": " + (is_covered_buffered_image ? "True" : "False"));
+            if (is_covered_buffered_image == false) {
                 bufferedImageCounter++;
             }
         }
-        System.out.println("\n    total missed branches : " + bufferedImageCounter + ", This means a coverage of : " + (1 - (double)bufferedImageCounter / bufferedImage_total_branch )*100 + " %");
+        System.out.println("\n    total missed branches : " + bufferedImageCounter + ", This means a coverage of : "
+                + (1 - (double) bufferedImageCounter / bufferedImage_total_branch) * 100 + " %");
 
-      
         System.out.println("\n\n    For function getRasterData:");
-        int counter = 0;
-        int total_branch = 54;
-        for (int i = 1; i <= total_branch; i++) {
+        int getImageCounter = 0;
+        int getImageTotal_branch = 54;
+        for (int i = 1; i <= getImageTotal_branch; i++) {
             boolean is_covered = branchCoverage_run_getImageInfo.getOrDefault(i, false);
             System.out.println("    Branch " + i + ": " + (is_covered ? "True" : "False"));
             if (is_covered == false) {
-                counter++;
+                getImageCounter++;
             }
         }
-        System.out.println("\n    total missed branches : " + counter + ", This means a coverage of : " + (1 - (double) counter / total_branch) + " %");
+        System.out.println("\n    total missed branches : " + getImageCounter + ", This means a coverage of : "
+                + (1 - (double) getImageCounter / getImageTotal_branch) + " %");
     }
-
 
     public static void printCoverageReport() {
         System.out.println("\n\nBranch Coverage Report: ");
         print_run();
-        print_run_getImageInfo();
     }
 
 }
